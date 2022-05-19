c98688af462e1d0c1badfcf75efcddac6987c86a
rferritorro <rferritorro@gmail.com>
2022-05-19 16:17:40 +0200

    Estructura del modulo home completada

--- backend/module/home/controller/controller_home.class.php
+++ backend/module/home/controller/controller_home.class.php
@@ -1,16 +1,13 @@
 <?php
 #This has to connect to db for send the data to front-end
 class controller_home {
-    function view() {
-        common::load_view('top_page_home.php', Home_view . '/home.html');
-    }
-    function carousel() {
-        echo json_encode(common::load_model('home_model', 'get_carousel'));
-    }
-    function tipo() {
-        echo json_encode(common::load_model('home_model', 'get_tipos'));
-    }
-    function categoria() {
-        echo json_encode(common::load_model('home_model', 'get_categoria'));
-    }
+    // function carousel() {
+    //     echo json_encode(common::load_model('home_model', 'get_carousel'));
+    // }
+    // function tipo() {
+    //     echo json_encode(common::load_model('home_model', 'get_tipos'));
+    // }
+    // function categoria() {
+    //     echo json_encode(common::load_model('home_model', 'get_categoria'));
+    // }
 }
